33ae914b191fab6ab76675e2a554dd97a9fcacbc
YuraPerebzyak <yura.perebzyak.231998@gmail.com>
2024-11-09 12:24:08 +0200

    add hw7

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,28 @@
+import styles from "./SearchBox.module.css";
+import { useSelector } from "react-redux";
+import { changeFilter, selectNameFilter } from "../../redux/filter/filterSlice";
+import { useDispatch } from "react-redux";
+
+const SearchBox = () => {
+  const dispatch = useDispatch();
+  const searchValue = useSelector(selectNameFilter);
+
+  const handleSearchChange = (event) => {
+    dispatch(changeFilter(event.target.value));
+  };
+
+  return (
+    <div className={styles.box}>
+      <div className={styles.container}>
+        <p className={styles.label}>Find contacts by name</p>
+        <input
+          className={styles.input}
+          type="text"
+          onChange={handleSearchChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBox;
